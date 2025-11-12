ba5b69b1714148e74e2c77324d4ca27976c35025
TadjellTheAlchemist <76237371+tadjelllol@users.noreply.github.com>
2025-11-11 17:17:21 -0800

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -434,6 +434,7 @@ export default {
             .setFooter({ text: `Announced by ${interaction.user.username}` })
 
           await channel.send({
+            content: "@everyone",
             embeds: [announceEmbed],
             files: [
               {
